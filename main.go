261ed97c9706adda7a96db560c5f855203ab13db
greytabby <munchkin.coco@gmail.com>
2021-02-28 13:05:47 +0900

    Add app engine deploy configuration

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"os"
 
 	"cloud.google.com/go/firestore"
 	"github.com/gin-contrib/cors"
@@ -11,7 +12,32 @@ import (
 	"golang.org/x/xerrors"
 )
 
+type Config struct {
+	allowOrigin string
+	port        string
+}
+
+var appConfig = Config{
+	allowOrigin: "http://localhost:3000",
+	port:        "7777",
+}
+
+func getEnvOrDefault(name string, defaultValue string) string {
+	envValue := os.Getenv(name)
+	if envValue == "" {
+		return defaultValue
+	}
+	return envValue
+}
+
+func loadConfig() {
+	appConfig.allowOrigin = getEnvOrDefault("ALLOW_ORIGIN", "http://localhost:3000")
+	appConfig.port = getEnvOrDefault("PORT", "7777")
+}
+
 func main() {
+	loadConfig()
+
 	// create router
 	ctx := context.Background()
 	fsClient, err := createFireStoreClient(ctx)
@@ -21,12 +47,12 @@ func main() {
 	}
 	r := gin.Default()
 	config := cors.DefaultConfig()
-	config.AllowOrigins = []string{"http://localhost:3000"}
+	config.AllowOrigins = []string{appConfig.allowOrigin}
 	r.Use(cors.New(config))
 	r.GET("/channels", rssContoroller.GetChannels)
 	r.POST("/channels", rssContoroller.PostChannel)
 	r.GET("/channels/:id/feeds", rssContoroller.GetChannelFeeds)
-	r.Run(":7777")
+	r.Run(":" + appConfig.port)
 }
 
 func createFireStoreClient(ctx context.Context) (*firestore.Client, error) {
